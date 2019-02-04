1ee9f5c0b853a1836d387a2aa9eba498b19e405b
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 22:24:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(' and ')
   elsif array.length == 3
-  array = array.insert(2,'and ')
+   array = array.insert(2,'and ')
     array = array.join(', ')
   else   
   array.join(', ')
