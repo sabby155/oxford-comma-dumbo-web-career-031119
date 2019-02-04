a242a35a30036a2987de5d8a326cd6846c08b918
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 22:26:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join(' and ')
   elsif array.length == 3
+    last = array.pop()
     array = array.join(', ')
-    array.pop()
-
+    array
     # array = array.insert(2,'and ')
     # array = array.join(', ')
   else   
