21ca51907c5d91c97ba643408c9f4f89c9aedca2
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 22:25:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,11 @@ def oxford_comma(array)
   if array.length == 2
     array.join(' and ')
   elsif array.length == 3
-   array = array.insert(2,'and ')
     array = array.join(', ')
+    last = array.pop()
+    array
+    # array = array.insert(2,'and ')
+    # array = array.join(', ')
   else   
   array.join(', ')
   end 
