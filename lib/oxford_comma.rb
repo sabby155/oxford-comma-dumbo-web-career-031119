9c9ff79a715d81bbf0764ed90522b31854c9f2bc
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 22:30:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(' and ')
+  elsif array.length >= 3
+    last = array.pop()
+    array = array.insert(-1, "and #{last}")
+    array = array.join(', ')
+  else   
+  array.join(', ')
+  end 
 end
